ee4c0602444458aca23889c247f210f17b999a75
Podcakero <business.podcake@gmail.com>
2019-06-29 23:03:25 -0400

    added the pic as an attachment

--- conar.js
+++ conar.js
@@ -7,13 +7,15 @@ client.once('ready', () => {
 
 // Create an event listener for new guild members
 client.on('guildMemberAdd', member => {
-    // Send the message to a designated channel on a server:
-    const channel = member.guild.channels.find(ch => ch.name === 'general');
-    // Do nothing if the channel wasn't found on this server
-    if (!channel) 
-        return;
-    // Send the message, mentioning the member
-    channel.send(`${member} https://cdn.discordapp.com/attachments/524075147783569429/594707295204671508/welcome_to_yo_MAMA.jpg`);
-  });
+	// Send the message to a designated channel on a server:
+	const channel = member.guild.channels.find(ch => ch.name === 'general');
+	// Do nothing if the channel wasn't found on this server
+	if (!channel)
+		return;
+	// Send the message, mentioning the member
+
+	const rules = new Discord.Attachment('https://cdn.discordapp.com/attachments/524075147783569429/594707295204671508/welcome_to_yo_MAMA.jpg');
+	channel.send(`${member}`, rules);
+});
 
 client.login('NTc1Nzg5MTQyNDkxOTIyNDYz.XRgXDA.s6B4hP1xZMtTA1Pl2pIus0WCkYk');
